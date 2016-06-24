614b52edbb390b67187a1874f8654d1de8135748
Ankit Wasankar <ankit.wasankar12@gmail.com>
2016-06-24 09:09:24 +0530

    Code indented - Radix Sort

--- sorting_algorithms/RadixSort.py
+++ sorting_algorithms/RadixSort.py
@@ -65,5 +65,5 @@ def radix_sort(unsorted_list):
 
 
 # Sample example
-unsorted_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+unsorted_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 print radix_sort(unsorted_list)
